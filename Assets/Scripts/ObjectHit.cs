6c61ee6e308f4dea083b258851324e06b10fab6c
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-19 22:34:28 +0530

    changes

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Object : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other) {
+        GetComponent<MeshRenderer>().material.color = Color.black ;
         Debug.Log("That's a Hit!") ;
    }
 }
