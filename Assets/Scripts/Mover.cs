62fca05a1f265509a2ee9481184171b15eab316f
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-19 14:12:48 +0530

    Added Mover.cs script

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+using Unity.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]float moveSpeed = 10f;
+    void Start() {
+
+    }
+
+    void Update() {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed ; 
+        float yValue = 0f ;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed ;
+        transform.Translate(xValue, yValue, zValue) ;
+    }
+}
