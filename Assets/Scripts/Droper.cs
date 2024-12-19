928cc4e2deb995255174046b78beb7e38261e5b2
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-19 23:36:37 +0530

    first commit

--- Assets/Scripts/Droper.cs
+++ Assets/Scripts/Droper.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class Droper : MonoBehaviour
 {
     [SerializeField] float timeToWait ;
+    MeshRenderer myMeshRenderer ;
+    Rigidbody myRigidBody ;
     void Start()
     {
-        
+        myMeshRenderer = GetComponent<MeshRenderer>() ;
+        myRigidBody = GetComponent<Rigidbody>() ;
+        myMeshRenderer.enabled = false ;
+        myRigidBody.useGravity = false ;
     }
 
     void Update()
     {
         if(Time.time > timeToWait) {
-            Debug.Log("Lookout!");
+            myMeshRenderer.enabled = true ;
+            myRigidBody.useGravity = true ;
         }
     }
 }
