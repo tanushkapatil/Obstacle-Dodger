e3d6d4748f162ad12069c51f3cad8941c341230b
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-19 22:46:07 +0530

    changes

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,5 @@ public class Object : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other) {
         GetComponent<MeshRenderer>().material.color = Color.black ;
-        Debug.Log("That's a Hit!") ;
    }
 }
