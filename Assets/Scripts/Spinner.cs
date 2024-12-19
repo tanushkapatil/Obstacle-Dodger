c6098fafebba709d62912836ac58ec95f6bc0ad6
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-20 00:01:05 +0530

    first commit

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float xAngle = 0f ;
+    [SerializeField] float yAngle = 0f ;
+    [SerializeField] float zAngle = 0f ;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Rotate(xAngle , yAngle , zAngle) ;
+    }
+}
