7e26aa4f2df0264176910c7a17d61a9515d9bd43
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-19 21:57:36 +0530

    Changes

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,10 +5,18 @@ public class Mover : MonoBehaviour
 {
     [SerializeField]float moveSpeed = 10f;
     void Start() {
-
+        PrintInstructions() ;
     }
 
     void Update() {
+        MovePlayer() ;
+    }
+
+    void PrintInstructions() {
+        Debug.Log("Welcome to the Game!");
+    }
+
+    void MovePlayer() {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed ; 
         float yValue = 0f ;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed ;
